aeb4c0e1340b9e5f26dfde404b0b9baf907cab60
RakeshBatra <raakesh.batra@rediffmail.com>
2019-01-26 14:02:00 +0000

    Intialized Sepolicy for Colt
    Thanks to LineageOS
    
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -1,11 +1,8 @@
 #
 # This policy configuration will be used by all qcom products
-# that inherit from Lineage
+# that inherit from Colt
 #
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/private
-
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/common \
-    device/lineage/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/colt/sepolicy/qcom/common \
+    device/colt/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
