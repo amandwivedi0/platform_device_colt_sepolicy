0f17e1d3bd8862964e850e5d18397f1f00f5b1b1
Michael Bestas <mkbestas@lineageos.org>
2021-10-07 22:04:41 +0300

    sepolicy: Switch to SYSTEM_EXT_{PUBLIC,PRIVATE}_SEPOLICY_DIRS
    Fixes:
    warning: BOARD_PLAT_PRIVATE_SEPOLICY_DIR has been deprecated.
        Use SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS instead.
    warning: BOARD_PLAT_PUBLIC_SEPOLICY_DIR has been deprecated.
        Use SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS instead.
    Change-Id: I752602079de8ff4c5370fe3ec861b8746838d878

--- legacy-common/sepolicy.mk
+++ legacy-common/sepolicy.mk
@@ -4,6 +4,6 @@
 #
 
 ifeq ($(TARGET_HAS_LEGACY_CAMERA_HAL1), true)
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/lineage/sepolicy/legacy-common/public/legacy-camera-hal1
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    device/colt/sepolicy/legacy-common/public/legacy-camera-hal1
 endif
